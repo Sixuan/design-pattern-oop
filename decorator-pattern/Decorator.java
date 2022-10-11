10152963d13fa06b3946d3d89f6ec3556d11b261
Sixuan <sixuan@giphy.com>
2022-10-11 17:15:09 -0400

    Update Decorator.java

--- decorator-pattern/Decorator.java
+++ decorator-pattern/Decorator.java
@@ -1,5 +1,11 @@
 public abstract class Decorator extends Component {
   Component component;
+  
+  public Decorator(Component component)
+  {
+      this.component = component;
+  }
+  
   //attach new behavior to Decorator w/o changing base Component
   public abstract void operationNew();
 }
@@ -9,10 +15,12 @@ public class DecoratorA extends Decorator {
     public void operation()
     {
       //operation on Object ComponentA
+      System.out.println("DecoratorA: operation");
     }
  
     private void operationNew()
     {
       //NEW operation on Object DecoratorA
+      System.out.println("DecoratorA: NEW operation");
     }
 }
