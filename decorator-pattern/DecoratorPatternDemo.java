3bcf4c9aa6e17ebffea2edcd89fc783eb8fab317
Sixuan <sixuan@giphy.com>
2022-10-11 17:26:45 -0400

    Update DecoratorPatternDemo.java

--- decorator-pattern/DecoratorPatternDemo.java
+++ decorator-pattern/DecoratorPatternDemo.java
@@ -9,16 +9,16 @@ public class DecoratorPatternDemo {
 	{
 		// Creating an object of ComponentA and ComponentB
 		ComponentA comA = new ComponentA();
-    ComponentB comB = new ComponentB();
+		ComponentB comB = new ComponentB();
 
-    comA.operation(); // ComponentA: operation
-    comB.operation(); // ComponentB: operation
-    
-    
-    // Creating an object of DecoratorA
-    DecoratorA decorA = new Decorator(new ComponentA());
-    decorA.operation(); // DecoratorA: operation
-    decorA.operationNew(); // DecoratorA: NEW operation
+		comA.operation(); // ComponentA: operation
+		comB.operation(); // ComponentB: operation
+
+
+		// Creating an object of DecoratorA
+		DecoratorA decorA = new Decorator(new ComponentA());
+		decorA.operation(); // DecoratorA: operation
+		decorA.operationNew(); // DecoratorA: NEW operation
 
 	}
 }
