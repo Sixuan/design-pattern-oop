e0349b8363bf4cfa7687d428b9e565262dc872a0
Sixuan <sixuan@giphy.com>
2022-10-11 17:16:12 -0400

    Update Component.java

--- decorator-pattern/Component.java
+++ decorator-pattern/Component.java
@@ -5,11 +5,13 @@ public abstract class Component {
 public class ComponentA extends Component {
   public void operation() {
     //operation on Object ComponentA
+    System.out.println("ComponentA: operation");
   }  
 }
 
 public class ComponentB extends Component {
   public void operation() {
     //operation on Object ComponentB
+    System.out.println("ComponentB: operation");
   }  
 }
